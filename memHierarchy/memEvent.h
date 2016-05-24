23893799a1bfded7b9c3627249f5c4b9eeff3829
Ramyad Hadidi <rhadidi@gatech.edu>
2016-05-24 16:49:02 -0400

    commenting initialization hmcType

--- memHierarchy/memEvent.h
+++ memHierarchy/memEvent.h
@@ -255,6 +255,10 @@ public:
 	instPtr_	    = 0;
 	vAddr_		    = 0;
         inProgress_         = false;
+// #ifdef USE_VAULTSIM_HMC
+//         hmcInstType_ = 0;
+//         hmcTransId_ = 0;
+// #endif
     }
 
     /** return the original event that caused a NACK */
